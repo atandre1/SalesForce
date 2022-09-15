b3bc6591fb8dfd4f3ce8fbcbeb2bc56d3ffce773
atandre1 <atandrei@protonmail.com>
2022-09-15 13:49:59 +0300

    Updates added

--- src/test/java/tests/AccountCreationTest.java
+++ src/test/java/tests/AccountCreationTest.java
@@ -6,6 +6,11 @@ public class AccountCreationTest extends BaseTest{
 
     @Test
     public void createAccount() {
-
+        loginPage.open();
+        loginPage.login("atandrei@yahoo.com", "tms2022tms");
+        newAccountPage.open();
+        newAccountPage.createAccount();
+        newAccountPage.create("Warm", "Zubik007", "+375295145268", "007",
+                "zubik.cia.gov", "Private", "Government", "Washington DC, 2341");
     }
 }
